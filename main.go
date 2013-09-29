8402c28373f59f79bb3b2e98cf4e284ba71e1115
Edward Muller <edwardam@interlix.com>
2013-09-29 11:02:36 -0700

    use env var for duration

--- main.go
+++ main.go
@@ -3,10 +3,13 @@ package main
 import (
 	"time"
 	"fmt"
+	"github.com/freeformz/shh/utils"
 )
 
+
 func main() {
-	ticker := time.Tick(100 * time.Millisecond)
+	var duration = utils.GetEnvWithDefaultDuration("DURATION", "100ms")
+	ticker := time.Tick(duration)
 	for t := range ticker {
 		fmt.Println("Tick: ", t)
 	}

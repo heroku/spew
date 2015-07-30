745062264cf242508a5f09d1da861e8ce15de45d
Andrew Gwozdziewycz <apg@heroku.com>
2015-07-29 23:47:53 -0700

    Add msglen and easier to use rate.

--- main.go
+++ main.go
@@ -1,55 +1,83 @@
 package main
 
 import (
+	"fmt"
 	"log"
-	"log/syslog"
+	"math/rand"
 	"os"
+	"strconv"
+	"strings"
 	"time"
 )
 
-func main() {
-	var i = 0
+const (
+	defaultMsgSize = 32
+	defaultSeed    = 8675309
+	defaultRate    = 1 * time.Second
+)
+
+var bytes = []byte("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()-_=+{}[]|\\;:'<>,./?")
+
+func randStr(buf []byte) string {
+	for i := 0; i < len(buf); i++ {
+		buf[i] = bytes[rand.Intn(len(bytes))]
+	}
+	return string(buf)
+}
+
+func parseRate(rate string) (time.Duration, error) {
 	var duration time.Duration
-	var sl *log.Logger
-	longString := "0123456789"
-
-	long := os.Getenv("LONG") != ""
-
-	if long {
-		for i := 0; i < 12; i++ {
-			longString = longString + "-" + longString
+	var err error
+	num := int64(1)
+	bits := strings.Split(rate, "/")
+	if len(bits) == 2 {
+		duration, err = time.ParseDuration(bits[1])
+		if err != nil {
+			return defaultRate, err
 		}
-		longString = longString + "END"
+		num, err = strconv.ParseInt(bits[0], 10, 32)
+		if err != nil {
+			return defaultRate, err
+		}
+	} else {
+		num = 1
+		duration, err = time.ParseDuration(rate)
 	}
 
-	log.SetFlags(log.Ldate | log.Lmicroseconds)
+	return time.Duration(int64(duration) / num), nil
+}
 
-	duration, err := time.ParseDuration(os.Getenv("DURATION"))
+func main() {
+	var i = 0
+	var buf []byte
+
+	// Repeatable payloads.
+	if seed, err := strconv.ParseInt(os.Getenv("SEED"), 10, 32); err == nil {
+		rand.Seed(seed)
+	} else {
+		rand.Seed(defaultSeed)
+	}
+
+	sleepTime, err := parseRate(os.Getenv("RATE"))
 	if err != nil {
 		log.Println("Duration Parsing: ", err)
 		log.Println("Continuing w/o duration")
 	}
 
-	if os.Getenv("SYSLOG") != "" {
-		sl, err = syslog.NewLogger(syslog.LOG_ALERT|syslog.LOG_USER, log.Ldate|log.Lmicroseconds)
-		if err != nil {
-			log.Println("Error setting up syslog: ", err)
-			log.Println("Continuing w/o syslog")
-		}
+	mlen, err := strconv.ParseInt(os.Getenv("MSGSIZE"), 10, 32)
+	if err == nil && mlen > 0 {
+		buf = make([]byte, mlen)
+	} else {
+		log.Println("Unable to parse MSGSIZE, defaulting to MSGSIZE of", defaultMsgSize)
+		buf = make([]byte, defaultMsgSize)
 	}
 
 	for {
 		i++
-		if duration > 0 {
-			time.Sleep(duration)
+		if sleepTime > 0 {
+			time.Sleep(sleepTime)
 		}
-		if long {
-			log.Println("Spew: ", i, len(longString), longString)
-		} else {
-			log.Println("Spew: ", i)
-		}
-		if sl != nil {
-			sl.Println("Syslog Spew: ", i)
-		}
+
+		fmt.Println(i, "spews", randStr(buf))
 	}
 }
